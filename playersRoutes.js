b16effcc2d31d779a49484b80a686cefad4e0eed
ThonyMahoni <atm-3@msn.com>
2025-07-27 01:55:06 +0200

    Update playersRoutes.js

--- playersRoutes.js
+++ playersRoutes.js
@@ -1,8 +1,7 @@
 import express from 'express';
-import { PrismaClient } from '@prisma/client';
+import prisma from './src/prisma.js'; 
 
 const router = express.Router();
-const prisma = new PrismaClient();
 
 router.get('/', async (req, res) => {
   try {
