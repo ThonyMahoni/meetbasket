f7e36e3601bc9202ad5287ab60fb3bfe58eb767e
ThonyMahoni <atm-3@msn.com>
2025-07-31 00:41:23 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import { LocationProvider } from './contexts/LocationContext';
 import CookieConsent from './components/CookieConsent';
 import './index.css';
 
+
+
 function App() {
   return (
     <>
@@ -23,6 +25,9 @@ function App() {
 
 export default App;
 
+
+
+
 // ✅ Service Worker Registrierung
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
@@ -34,5 +39,35 @@ if ('serviceWorker' in navigator) {
       .catch((err) => {
         console.error('[SW] Registrierung fehlgeschlagen:', err);
       });
+  });
+}
+
+//Notification.requestPermission();
+if ('Notification' in window && 'serviceWorker' in navigator) {
+  window.addEventListener('load', async () => {
+    const permission = await Notification.requestPermission();
+    if (permission === 'granted') {
+      console.log('✅ Benachrichtigungen erlaubt');
+
+      const registration = await navigator.serviceWorker.ready;
+
+      const subscription = await registration.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: 'BDvgrOdRZhTRkJoJ0OFHbCTiedN4ltKHHoiywON399mge80E7NrYbYD8982jfDzVWmi9Ah2JQ7VE9IoDHEbWEh8',
+      });
+
+      // Sende die subscription an dein Backend:
+      await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/subscribe`, {
+        method: 'POST',
+        body: JSON.stringify(subscription),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      console.log('📬 Abo erfolgreich registriert', subscription);
+    } else {
+      console.warn('🔕 Benachrichtigungen nicht erlaubt');
+    }
   });
 }
