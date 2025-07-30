97f633d883e4f80ca1cec54e61578f85bd3093e0
ThonyMahoni <atm-3@msn.com>
2025-07-30 12:53:06 +0200

    Update profile.js

--- routes/profile.js
+++ routes/profile.js
@@ -7,7 +7,7 @@ import multer from 'multer';
 import path from 'path';
 import fs from 'fs';
 import NodeCache from 'node-cache';
-const cache = new NodeCache({ stdTTL: 180 }); // z. B. 3 Min Cache
+const cache = new NodeCache({ stdTTL: 180 }); // ca. 2min Cache
 
 
 
